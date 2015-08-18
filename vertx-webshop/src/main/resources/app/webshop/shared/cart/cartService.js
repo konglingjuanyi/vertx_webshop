32d2fd4e4fa4cd5722e9d46cd9cecf5e1ad2ecaf
rstraub <roy.straub1991@gmail.com>
2015-08-18 13:05:53 +0200

    Finalize cart and cart total
    -Refactored the cart layout slightly
    -Added some utility to the shoppingcart service, it controls logic to
    calculate its total price.
    -Adjusted some naming

--- vertx-webshop/src/main/resources/app/webshop/shared/cart/cartService.js
+++ vertx-webshop/src/main/resources/app/webshop/shared/cart/cartService.js
@@ -1,26 +1,44 @@
 app.factory('ShoppingCart', function(){
     return {
-        cartProducts: [],
-        
-        cartTotal: 0,
-        
+        products: [],
+
+        total: 0,
+
         addProduct: function(product){
-            //Check if the product is already in the cart
+            //Check if the product is already in the cart, if it is the amount is increased
             var productExists = false;
-            if(this.cartProducts.indexOf(product) > -1){
+            var cartProduct = {};
+            for(index = 0; index < this.products.length; index++){
+              if(this.products[index].product === product){
                 productExists = true;
+                cartProduct = this.products[index]; 
+                cartProduct.amount++;
+                break;
+              }
             }
-            //If it is not, add it, and add it to the total
+            //If the cart item does not exist, add it, and add it to the collection
             if(!productExists){
-                this.cartProducts.push(product);
-                this.cartTotal += product.price;
+                cartProduct.product = product;
+                cartProduct.amount = 1;
+                this.products.push(cartProduct);   
             }
+            cartProduct.total = cartProduct.amount * cartProduct.product.price;
+            //Update the total cart value
+            this.calculateTotal();
         },
-        
+
         removeProduct: function(index){
-            //TODO 
+            //TODO
             //Remove the item
             //Subtract the item's costs
+        },
+        
+        calculateTotal: function(){
+            var total = 0;
+            for(index = 0; index < this.products.length; index++){
+                total += this.products[index].total;
+            }
+            this.total = total;
         }
     }
 });
